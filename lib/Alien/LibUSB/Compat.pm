16b5d260625d067d0b6c4ea9da6994fb5af243b3
Henrik Brix Andersen <henrik@brixandersen.dk>
2013-09-08 12:30:59 +0200

    Clean up documentation.

--- lib/Alien/LibUSB/Compat.pm
+++ lib/Alien/LibUSB/Compat.pm
@@ -8,7 +8,7 @@ use parent 'Alien::Base';
 
 =head1 NAME
 
-Alien::LibUSB::Compat - installing, finding and using libusb-compat
+Alien::LibUSB::Compat - Alien package for libusb-0.1
 
 =head1 VERSION
 
@@ -21,10 +21,15 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Please see L<Alien> for the manifesto of the Alien namespace.
+C<Alien::LibUSB::Compat> is an L<Alien> package for libusb-0.1.
 
-In short C<Alien::LibUSB::Compat> can be used to detect and get configuration
-settings from an installed libusb-compat.
+On installation, C<Alien::LibUSB::Compat> will try to detect whether
+or not libusb-0.1 is already available on the system. If not, a
+libusb-0.1 compatible library will be installed. The compatibility
+library depends on L<Alien::LibUSBx>.
+
+C<Alien::LibUSB::Compat> is a simple extension to L<Alien::Base>,
+please see that for API references.
 
 =head1 AUTHOR
 
@@ -32,16 +37,18 @@ Henrik Brix Andersen, C<< <brix at cpan.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-alien-libusb-compat at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Alien-LibUSB-Compat>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+Please report any bugs or feature requests to
+C<bug-alien-libusb-compat at rt.cpan.org>, or through the web
+interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Alien-LibUSB-Compat>.
+I will be notified, and then you'll automatically be notified of
+progress on your bug as I make changes.
 
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
     perldoc Alien::LibUSB::Compat
-
 
 You can also look for information at:
 
@@ -65,47 +72,23 @@ L<http://search.cpan.org/dist/Alien-LibUSB-Compat/>
 
 =back
 
+=head1 SEE ALSO
+
+L<Alien>, L<Alien::Base>, L<Alien::LibUSBx>, L<perlartistic>,
+L<perlgpl>
 
 =head1 LICENSE AND COPYRIGHT
 
 Copyright 2013 Henrik Brix Andersen.
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
+This program is free software; you can redistribute it and/or modify
+it under the terms of either:
 
-L<http://www.perlfoundation.org/artistic_license_2_0>
+    a) the GNU General Public License as published by the Free
+    Software Foundation; either version 1, or (at your option) any
+    later version, or
 
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+    b) the "Artistic License" which comes with this Kit.
 
 =cut
 
